4c2d2533292b99e7eb2e403b494664a6265a4bbf
Caio Couto <coutocaio3@gmail.com>
2023-04-03 08:56:27 -0300

    Abstracted network icons from footer.

---
+++ src/components/NetworkIcons/NetworkIcons.jsx
@@ -0,0 +1,35 @@
+import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa'
+
+import styles from './networkIcons.module.css'
+
+const links = [
+    {
+        label: 'Github Link',
+        url: 'https://www.github.com/CaioCouto',
+        icon: FaGithub
+    },
+    {
+        label: 'Frontend Mentor Link',
+        url: 'https://www.frontendmentor.io/profile/CaioCouto',
+        icon: FaExternalLinkAlt
+    },
+]
+
+function NetworkIcons() {
+    return (   
+        <div className={ styles["NetworkIcons-links"] }>
+            {
+                links.map((link, index) => (
+                    <a key={ index } className={ styles["NetworkIcons-link"] } href={ link.url } target='blank_'>
+                        <p style={{ display: 'none' }}>{ link.label }</p>
+                        <link.icon 
+                            className={ `${styles["NetworkIcons-icon"]} ${styles["NetworkIcons-icon-menu"]}` }
+                        />
+                    </a>
+                ))
+            }
+        </div>
+    )
+}
+
+export default NetworkIcons
---
+++ src/components/NetworkIcons/index.js
@@ -0,0 +1,3 @@
+import NetworkIcons from "./NetworkIcons"
+
+export default NetworkIcons
